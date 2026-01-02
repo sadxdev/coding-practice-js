e668570ffd48eb616fc1b43c3448f87ca987440b
sadxdev <sadikxdev@gmail.com>
2026-01-02 16:45:57 +0530

    #961-N-repeated-element-size-2N-array

---
+++ leetcode-daily-problem/961-N-repeated-element-size-2n-array.js
@@ -0,0 +1,36 @@
+/*You are given an integer array nums with the following properties:
+
+nums.length == 2 * n.
+nums contains n + 1 unique elements.
+Exactly one element of nums is repeated n times.
+Return the element that is repeated n times.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,1,2,5,3,2]
+Output: 2
+Example 3:
+
+Input: nums = [5,1,5,2,5,3,5,4]
+Output: 5*/
+
+var repeatedNTimes = function (nums) {
+  const seen = new Set();
+
+  for (let num of nums) {
+    if (seen.has(num)) {
+      return num;
+    }
+    seen.add(num);
+  }
+};
+
+console.log(repeatedNTimes([1, 2, 3, 3])); // 3
+console.log(repeatedNTimes([2, 1, 2, 5, 3, 2])); // 2
+console.log(repeatedNTimes([5, 1, 5, 2, 5, 3, 5, 4])); // 5
